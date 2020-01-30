532b96682bcdac9df7d32cf1950c2d53536defde
Sam Bryant <bryantsj@gmail.com>
2020-01-30 22:44:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
-  turn_count.each do |what_turn|
-  puts "This is the turn number #{turn_count}"
-  turn_count += 1
-end
-end
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || "O"
+      counter += 1
+    end 
